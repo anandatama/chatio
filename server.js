1bf1dd3ac593a4acab0eca39f8cdb7359ad602fd
Ananda Vickry Pratama <anandapratama170199@gmail.com>
2018-01-18 00:35:37 +0700

    Adding node

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server);
+
+server.listen(process.env.PORT || 5000 );
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
